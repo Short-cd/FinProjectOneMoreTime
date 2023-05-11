36f6a82eac513ddf2a3735b59d3cfc486226dbcc
Short-cd <kcrummel907@gmail.com>
2023-05-11 11:51:48 -0500

    szfbc

--- src/main/java/com/csafinal/basedefense/data/LivingThingData.java
+++ src/main/java/com/csafinal/basedefense/data/LivingThingData.java
@@ -2,5 +2,13 @@ package com.csafinal.basedefense.data;
 
 import com.csafinal.basedefense.DropApp;
 
-public record LivingThingData(DropApp.Type type, int hp, String name, int damage, double speed) {
+public record LivingThingData(
+        DropApp.Type type,
+        int hp,
+        String name,
+        int damage,
+        double speed,
+        String imageName,
+        double interval
+) {
 }
